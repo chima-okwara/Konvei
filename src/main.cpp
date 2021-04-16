a000f482b85f4872b254e17ce8011e2e129da680
chima-okwara <cokwara82@gmail.com>
2021-04-16 11:35:52 +0100

    Minor changes: improved project to make use of only one infrared sensor.

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 //*FILE DESC:       Source file for Konvei.
 //*FILE VERSION:    0.1.0
 //*FILE AUTHOR:     Chimaroke Okwara
-//*LAST MODIFIED:   
+//*LAST MODIFIED:
 //*LICENSE:         MIT License
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 #include <Arduino.h>
@@ -18,19 +18,20 @@ LiquidCrystal lcd (7,8,9,10,11,12);
 const uint8_t buzzer = 15;
 
 //Infrared sensors:
-irSensor ir1(5, LOW), ir2(6, LOW);
+irSensor ir(5, LOW);
 
-//Ultrasonic sensor
+//Ultrasonic sensor:
 ultrasonicSensor ultra(13, 14);
 
 //Motor pin:
 const uint8_t motor = 5;
 
 //Variables for calculating object height:
-float itemDistance;     //Distance between the item and the ultrasonic sensor
-float refHeight = 30;   //Distance between the conveyor floor and the ultrasonic sensor
-float itemHeight = 0;   //Item calculated height i.e   refHeight-itemDistance
+float itemDistance;     //Distance between the item and the ultrasonic sensor.
+float refHeight = 30;   //Distance between the conveyor floor and the ultrasonic sensor.
+float itemHeight = 0;   //Item calculated height i.e   refHeight-itemDistance.
 long totalCount = 0;    //Count of accepted objects.
+
 
 void setup();
 void loop();
@@ -38,23 +39,23 @@ void loop();
 
 void setup()
 {
-  //Initialise LCD screen
+  //Initialise LCD screen:
   lcd.begin(16, 2);
 
-  //Pin connected to buzzer
+  //Pin connected to buzzer:
   pinMode(buzzer,OUTPUT);
 
   //Pin connected to motor
   pinMode(motor,OUTPUT);
 
-  //  digitalWrite(motor,HIGH);
+   digitalWrite(motor,HIGH);
 
 
   //To deactivate LED on the Arduino Uno
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite (LED_BUILTIN, LOW);
 
-  //Print Welcome Message for 3 sec
+  //Print Welcome Message for
   lcd.setCursor(0, 0);
   lcd.print("Eichen(R)");
   delay(3000);
@@ -68,16 +69,14 @@ void setup()
   {
     lcd.scrollDisplayLeft();
     delay(200);
-  }  delay(3000);
+  }  delay(300);
 
   lcd.clear();
 
-  //Switch on the motor
+  lcd.setCursor(0, 0);
+  lcd.println("Starting motor...");
   digitalWrite(motor,LOW);
-
-  //Display message
-  lcd.setCursor(0,0);
-  lcd.print("MOTOR:   MOVING");
+  lcd.clear();
 
   //H stands for HEIGHT
   lcd.setCursor(0,1);
@@ -92,7 +91,7 @@ void setup()
 
 void loop()
 {
-  if (ir1.detect()) //when an object is detected by the irsenor1
+  if (ir.detect()) //when an object is detected by the irsenor1
   {
       itemDistance = ultra.getDistance_cm();
       itemHeight   = refHeight - itemDistance;
@@ -104,7 +103,7 @@ void loop()
       lcd.setCursor(3,1);
       lcd.print(itemHeight);
 
-      if ( itemHeight >= 20.0) //if the object height is up to or equals 20cm
+      if ((itemHeight >= 20.0) && ir.detect()) //if the object height is up to or equals 20cm
       {
          totalCount++;
          lcd.setCursor(11,1);
@@ -118,8 +117,8 @@ void loop()
 
       }
 
-      else  //if the object height is less than 20cm
-        //and the object is still in front of the sensors
+      else if ((itemHeight < 20.0) && ir.detect())  
+      //if the object height is less than 20cm and the object is still in front of the sensors
       {
 
             lcd.setCursor(9,1);
@@ -129,7 +128,7 @@ void loop()
             lcd.print("STOPPED");
             digitalWrite(buzzer, HIGH); //start buzzer
 
-            while (ir2.detect())      //As long as the item is not removed...
+            while (ir.detect())      //As long as the item is not removed...
               delay(100);             //...do nothing.
 
       }
